a7861a61addd1212083ef99b5eafa92773f3e01f
Lucas <contact@lucasquitman.fr>
2024-01-09 15:11:31 +0100

    Set up the task view & configure spb to interpret http method from forms

---
+++ src/main/java/com/esiea/todo/WebConfig.java
@@ -0,0 +1,24 @@
+package com.esiea.todo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UrlPathHelper;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        urlPathHelper.setRemoveSemicolonContent(false);
+        configurer.setUrlPathHelper(urlPathHelper);
+    }
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
+}
---
+++ src/main/resources/static/edit.js
@@ -0,0 +1,23 @@
+
+function showPopup(event) {
+    event.preventDefault();
+    var link = event.target;
+    var title = link.dataset.title;
+    var dueDate = link.dataset.duedate;
+    var description = link.dataset.description;
+    var status = link.dataset.status;
+    var id = link.dataset.id;
+    console.log(id, dueDate, "debug")
+    console.log(link.dataset, "debug2")
+    console.log(link.getAttribute('data-dueDate'), "debug3")
+    document.querySelector('#popup input[name="title"]').value = title;
+    document.querySelector('#popup input[name="dueDate"]').value = dueDate;
+    document.querySelector('#popup input[name="description"]').value = description;
+    document.querySelector('#popup input[name="id"]').value = id;
+    document.querySelector('#popup select[name="status"]').value = status;
+    document.getElementById('popup').style.display = 'block';
+}
+
+function hidePopup() {
+    document.getElementById("popup").style.display = "none";
+}
