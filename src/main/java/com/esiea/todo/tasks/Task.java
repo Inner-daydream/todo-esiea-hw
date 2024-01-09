68f720ebb986050479b20f23e88da6346a0bec4d
Lucas <contact@lucasquitman.fr>
2024-01-09 21:08:21 +0100

    allow tasks to be compared

--- src/main/java/com/esiea/todo/tasks/Task.java
+++ src/main/java/com/esiea/todo/tasks/Task.java
@@ -74,4 +74,15 @@ public class Task {
         this.dueDate = dueDate;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Task))
+            return false;
+        Task task = (Task) o;
+        return id.equals(task.id) && title.equals(task.title) && description.equals(task.description)
+                && status == task.status && dueDate.equals(task.dueDate);
+    }
+
 }
