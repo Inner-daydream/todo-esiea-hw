c68b4c6b6828c14a78e088b86d8dbe0b18fde63a
Lucas <contact@lucasquitman.fr>
2024-01-09 15:07:11 +0100

    Add Status and Task classes

---
+++ src/main/java/com/esiea/todo/tasks/Status.java
@@ -0,0 +1,7 @@
+package com.esiea.todo.tasks;
+
+public enum Status {
+    OPEN,
+    IN_PROGRESS,
+    DONE
+}
---
+++ src/main/java/com/esiea/todo/tasks/Task.java
@@ -0,0 +1,77 @@
+package com.esiea.todo.tasks;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String description;
+    private Status status;
+    private Date dueDate;
+
+    protected Task() {
+    }
+
+    public Task(String title, String description, Date dueDate) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = Status.OPEN;
+    }
+
+    public Task(String title, String description, Status status, Date dueDate) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.dueDate = dueDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+}
